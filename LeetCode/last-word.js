dd6abf359d642faf71544e1a019702476824eef0
pallavid08 <pallavid0808@gmail.com>
2022-01-12 16:36:08 +0100

    added one more

---
+++ LeetCode/last-word.js
@@ -0,0 +1,7 @@
+// Given a string s consisting of some words separated by some number of spaces, return the length of the last word in the string.
+
+// A word is a maximal substring consisting of non-space characters only.
+
+const lengthOfLastWord = (s) => {};
+
+console.log(s);
