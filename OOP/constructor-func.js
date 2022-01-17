9ca9545ff76090be6208092a2bccd3f15102edfa
pallavid08 <pallavid0808@gmail.com>
2022-01-17 22:46:13 +0100

    oop and leetcode

--- OOP/constructor-func.js
+++ OOP/constructor-func.js
@@ -1,10 +1,38 @@
 //Constructor Functions
 function Elf(name, weapon) {
+   console.log('this', this);
    this.name = name;
    this.weapon = weapon;
+   console.log('this', this);
 }
+
+Elf.prototype.attack = function () {
+   return 'attack with ' + this.weapon;
+};
+
+Elf.prototype.build = function () {
+   const self = this;
+   function building() {
+      return self.name + ' builds a house';
+   }
+   return building();
+};
 
 const peter = new Elf('Peter', 'stones');
 console.log(peter);
+console.log(peter.attack());
+console.log(peter.build());
+
 const sam = new Elf('Sam', 'fire');
 console.log(sam);
+console.log(sam.attack());
+
+const Elf1 = new Function(
+   'name',
+   'weapon',
+   `this.name = name;
+this.weapon = weapon;`
+);
+
+const sarah = new Elf1('Sarah', 'fireworks');
+// console.log(sarah);
