a59141265f1477738f0578575b42916b98d8cee5
pallavid08 <pallavid0808@gmail.com>
2022-01-22 12:30:15 +0100

    Renamed files

--- LeetCode/58.js
+++ LeetCode/58.js
@@ -1,3 +1,5 @@
+//Length of Last Word
+
 // Given a string s consisting of some words separated by some number of spaces, return the length of the last word in the string.
 
 // A word is a maximal substring consisting of non-space characters only.
